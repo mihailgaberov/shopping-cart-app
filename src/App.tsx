0dd3f2d30b5d38cc249bb2eee19846cac0001fe6
Mihail Gaberov <mihail.gaberov@olx.com>
2023-06-16 07:34:37 +0200

    :construction: Building the products page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
+import { Products } from './components/Products'
+import { useEffect, useState } from 'react'
+
+const API_URL = 'https://dummyjson.com/products'
+
 
 function App() {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    fetchData(API_URL)
+  }, [])
+
+
+  async function fetchData(url: string) {
+    try {
+      const response = await fetch(url)
+      if (response.ok) {
+        const data = await response.json();
+        // Process the data
+        console.log('>>> data: ', data)
+        setProducts(data.products)
+      } else {
+        // Handle the error
+        console.log('>>> error response',)
+      }
+    } catch (error) {
+      // Handle the error
+      console.log('>>> error: ', error)
+    }
+  }
 
   return (
     <>
       <Header />
       <main>
-        main content here...
+        <Products data={products} />
       </main>
       <Footer />
     </>
---
+++ src/components/Products/Products.tsx
@@ -0,0 +1,32 @@
+import { FunctionComponent } from 'react'
+import classes from './products.module.scss'
+
+type Product = {
+  id: number
+  title: string
+  price: number
+  thumbnail: string
+  image: string
+}
+
+interface Props {
+  data: Product[]
+}
+
+
+export const Products: FunctionComponent<Props> = ({ data }) => (
+  <div className={classes.productPage}>
+    <h1>Products</h1>
+
+    <div className={classes.container}>
+      {data.map(product => (
+        <div className={classes.product} key={product.id}>
+          <img src={product.thumbnail} alt={product.title} />
+          <h3>{product.title}</h3>
+          <p>Price: {product.price}</p>
+          <button>Add to Cart</button>
+        </div>
+      ))}
+    </div>
+  </div>
+)
