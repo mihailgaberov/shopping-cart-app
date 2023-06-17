086615a5185e77c702d24aefcfd13469432fdd65
Mihail Gaberov <mihail.gaberov@olx.com>
2023-06-17 19:26:45 +0200

    :white_check_mark: Adding tests for CartWidget component

--- src/components/CartWidget/CartWidget.test.tsx
+++ src/components/CartWidget/CartWidget.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { describe, expect, it } from 'vitest'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -13,5 +13,11 @@ describe('CartWidget', () => {
   it('renders correctly when products are added to the cart', () => {
     render(<BrowserRouter><CartWidget productsCount={3} /></BrowserRouter>)
     expect(screen.getByText('3')).toBeInTheDocument()
+  })
+
+  it('should go to the cart page when clicked', () => {
+    render(<BrowserRouter><CartWidget productsCount={3} /></BrowserRouter>)
+    fireEvent.click(screen.getByRole('button'))
+    expect(window.location.pathname).toBe('/cart');
   })
 })
