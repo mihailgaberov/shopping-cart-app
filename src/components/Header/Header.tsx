4d2acec3ea522c05c5ffcffb9c6133c8f203bda7
Mihail Gaberov <mihail.gaberov@olx.com>
2023-06-18 11:33:34 +0200

    :lipstick: Styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import useLocalStorageState from 'use-local-storage-state'
 
@@ -8,6 +8,26 @@ import { CartProps } from '../Products/Products.tsx'
 import classes from './header.module.scss'
 
 export const Header: FunctionComponent = () => {
+  useEffect(() => {
+    window.addEventListener("scroll", () => shrinkHeader(), false)
+
+    return () => {
+      window.removeEventListener("scroll", () => shrinkHeader())
+    }
+  }, [])
+
+  const shrinkHeader = () => {
+    const DISTANCE_FROM_TOP = 140
+    const headerElement = document.querySelector("header") as HTMLElement
+    const scrollY =
+      document.body.scrollTop || document.documentElement.scrollTop
+    if (scrollY > DISTANCE_FROM_TOP) {
+      headerElement.style.transition = "height 200ms ease-in"
+      headerElement.style.height = "100px"
+    } else {
+      headerElement.style.height = "150px"
+    }
+  }
   const [cart,] = useLocalStorageState<CartProps>('cart', {})
 
   const productsCount: number = Object.keys(cart || {}).length
