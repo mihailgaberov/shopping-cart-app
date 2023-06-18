46d853db90079ce4f7cb9b4a10eac51f0d770c79
Mihail Gaberov <mihail.gaberov@olx.com>
2023-06-18 13:31:54 +0200

    :sparkles: Reset scroll position to the top when enter the cart page

--- src/components/Cart/Cart.test.tsx
+++ src/components/Cart/Cart.test.tsx
@@ -2,11 +2,12 @@ import { fireEvent, render, screen } from '@testing-library/react'
 import { afterEach, beforeEach, describe, expect, it } from 'vitest'
 
 import { Cart } from "./index"
+import { BrowserRouter } from 'react-router-dom'
 
 describe('Cart', () => {
   beforeEach(() => {
     localStorage.setItem('cart', JSON.stringify({"2":{"id":2,"title":"iPhone X","description":"SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...","price":899,"discountPercentage":17.94,"rating":4.44,"stock":34,"brand":"Apple","category":"smartphones","thumbnail":"https://i.dummyjson.com/data/products/2/thumbnail.jpg","images":["https://i.dummyjson.com/data/products/2/1.jpg","https://i.dummyjson.com/data/products/2/2.jpg","https://i.dummyjson.com/data/products/2/3.jpg","https://i.dummyjson.com/data/products/2/thumbnail.jpg"],"quantity":1},"3":{"id":3,"title":"Samsung Universe 9","description":"Samsung's new variant which goes beyond Galaxy to the Universe","price":1249,"discountPercentage":15.46,"rating":4.09,"stock":36,"brand":"Samsung","category":"smartphones","thumbnail":"https://i.dummyjson.com/data/products/3/thumbnail.jpg","images":["https://i.dummyjson.com/data/products/3/1.jpg"],"quantity":1}}))
-    render(<Cart />)
+    render(<BrowserRouter><Cart /></BrowserRouter>)
   })
 
   afterEach(() => {
