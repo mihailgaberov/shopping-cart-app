2d31d8631e916db283df16822c13a33d7177ae1b
Mihail Gaberov <mihail.gaberov@olx.com>
2023-06-18 12:25:18 +0200

    :lipstick: Make header and its elements shrink on scroll down

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -19,13 +19,25 @@ export const Header: FunctionComponent = () => {
   const shrinkHeader = () => {
     const DISTANCE_FROM_TOP = 140
     const headerElement = document.querySelector("header") as HTMLElement
-    const scrollY =
-      document.body.scrollTop || document.documentElement.scrollTop
+    const logoElement = document.querySelectorAll("img")[0] as HTMLElement
+    const cartWidgetElement = document.querySelectorAll("img")[1] as HTMLElement
+    const productsCountElement = document.querySelector("span") as HTMLElement
+    const scrollY = document.body.scrollTop || document.documentElement.scrollTop
+
     if (scrollY > DISTANCE_FROM_TOP) {
       headerElement.style.transition = "height 200ms ease-in"
-      headerElement.style.height = "100px"
+      headerElement.style.height = "80px"
+      logoElement.style.transition = "height 200ms ease-in"
+      logoElement.style.height = "4rem"
+      cartWidgetElement.style.transition = "height 200ms ease-in"
+      cartWidgetElement.style.height = "2rem"
+      productsCountElement.style.transition = "font-size 200ms ease-in"
+      productsCountElement.style.fontSize = "1.5em"
     } else {
       headerElement.style.height = "150px"
+      logoElement.style.height = "6rem"
+      cartWidgetElement.style.height = "3rem"
+      productsCountElement.style.fontSize = "2em"
     }
   }
   const [cart,] = useLocalStorageState<CartProps>('cart', {})
